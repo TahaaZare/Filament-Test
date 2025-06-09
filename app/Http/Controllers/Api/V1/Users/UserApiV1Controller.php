9d420c843273d6042e4903dd1243d2e354617017
TahaaZare <tahaazre.dev@gmail.com>
2025-06-09 15:44:17 +0330

    UPDATE README

--- app/Http/Controllers/Api/V1/Users/UserApiV1Controller.php
+++ app/Http/Controllers/Api/V1/Users/UserApiV1Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1\Users;
 
 use App\Core\Api\API;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\Users\Collections\UserV1Collection;
 use App\Models\User;
 
 class UserApiV1Controller extends Controller
@@ -11,6 +12,6 @@ class UserApiV1Controller extends Controller
     public function users()
     {
         $data = User::all();
-        return API::success("Get all users", $data);
+        return API::success("Get all users", new UserV1Collection($data));
     }
 }
